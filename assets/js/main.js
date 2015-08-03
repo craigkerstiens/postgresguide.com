cb16664f28157b2717082b1c6f19fcffe0a716d4
Dustin Miller <dustin@sharepointexperts.com>
2015-08-03 02:23:27 -0500

    Updated mobile menu event handling to stay open properly on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,18 @@ $(document).ready(function(){
     $(".side li a.active ~ nav").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" +           $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
   });
+
+  var $elSiteNav = $('.site-nav');
+  $elSiteNav.on('touchstart', function(e) {
+  	$('.trigger').addClass('expanded');
+  });
+  $elSiteNav.on('mouseenter mouseleave', function(e) {
+  	$('.trigger').toggleClass('expanded');
+  });
+  $('body').on('click touchstart', function(e) {
+  	if (!$elSiteNav.is(e.target) && $elSiteNav.has(e.target).length === 0) {
+        $('.trigger').removeClass('expanded');
+    }
+  });
+
 });
